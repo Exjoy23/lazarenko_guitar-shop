30ce244418b4d69b2b1325f5efd23df867609426
Exjoy23 <exjoy23@gmail.com>
2021-09-15 22:24:56 +0300

    добавит шапку

---
+++ src/components/header/header.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './header.module.scss';
+import { Logo } from '../logo/logo';
+import { Link } from 'react-router-dom';
+import location from './location.svg';
+import search from './search.svg';
+import cart from './cart.svg';
+import guitar from './guitar.png';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.wrapper}>
+        <div className={styles.inner}>
+          <Logo primary />
+          <nav className={styles.nav}>
+            <ul className={styles.list}>
+              <li className={styles.item}>
+                <Link to="/">Каталог</Link>
+              </li>
+              <li className={styles.item}>
+                <Link to="/">Где купить?</Link>
+              </li>
+              <li className={styles.item}>
+                <Link to="/">О компании</Link>
+              </li>
+              <li className={styles.item}>
+                <Link to="/">Сервис-центры</Link>
+              </li>
+            </ul>
+            <ul className={classNames(styles.list, styles.items)}>
+              <li>
+                <Link className={styles.link} to="/">
+                  <img src={location} alt="Наши магазины" />
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link} to="/">
+                  <img src={search} alt="Поиск товаров" />
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link} to="/">
+                  <img
+                    className={styles.cart}
+                    src={cart}
+                    alt="Корзина товаров"
+                  />
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div className={styles.guitar}>
+        <img src={guitar} alt="Шестиструнная электрогитара" />
+      </div>
+    </header>
+  );
+}
+
+export { Header };
--- src/components/pages/main-page/main-page.jsx
+++ src/components/pages/main-page/main-page.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './main-page.module.scss';
+import { Header } from '../../header/header';
 
 function MainPage() {
-  return <div className={styles.wrapper}>Hello</div>;
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+    </div>
+  );
 }
 
 export { MainPage };
